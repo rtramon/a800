51dea70295d2fbee48efe55b438c21e8384793b4
rtramon <rtramon@gmail.com>
2024-08-11 18:26:32 +0200

    ann ewzo 6502 monitor

---
+++ ewoz.h
@@ -0,0 +1,16 @@
+//
+// Author: Rien Matthijsse
+//
+
+#ifndef _EWOZ_h
+#define _EWOZ_h
+#include <stdint.h>
+
+// #define EWOZ_START 0xFC00
+// #define EWOZ_SIZE 0x0330
+#define EWOZ_START 0xFC00
+#define EWOZ_SIZE 707
+extern const uint8_t ewoz_bin[];
+
+extern const uint8_t vectors_bin[];
+#endif
